3d56daaf90df7daeeb520fbc109a98581f5fbc82
JensBaumannDev <baumannjens@outlook.de>
2026-01-20 20:10:43 +0100

    change price position in food card, add new renderfunction in js

--- scripts/template.js
+++ scripts/template.js
@@ -21,18 +21,56 @@ function getFoodTemplate(food) {
   `;
 }
 
-function getFoodCategory(food) {
-  for (let index = 0; index < foodDataBase.length; index++) {
-    if (foodDataBase[index].category == "Burger") {
-      burgerRef.innerHTML += getFoodTemplate(foodDataBase[index]);
-    } else if (foodDataBase[index].category == "Pizza") {
-      pizzaRef.innerHTML += getFoodTemplate(foodDataBase[index]);
-    } else if (foodDataBase[index].category == "Nudeln") {
-      nuddlesRef.innerHTML += getFoodTemplate(foodDataBase[index]);
-    } else if (foodDataBase[index].category == "Beilagen") {
-      salatRef.innerHTML += getFoodTemplate(foodDataBase[index]);
+const categoryRefs = {
+  Burger: document.getElementById("burger-content"),
+  Pizza: document.getElementById("pizza-content"),
+  Nudeln: document.getElementById("nudles-content"),
+  Beilagen: document.getElementById("extra-content"),
+};
+
+function foodData() {
+  renderFoodItems(foodDataBase);
+}
+
+function getFoodTemplate(food) {
+  let formattedPrice = food.price.toFixed(2).replace(".", ",");
+  return `
+        <div class="food-card">
+            <div class="food-card-inner">
+                <h3>${food.name}</h3>
+                <p class="description">${food.description}</p>
+                <span class="price">${formattedPrice} €</span>
+            </div>
+            <button class="add-btn" onclick="addToBasket('${food.name}')">Hinzufügen</button>
+        </div>
+    `;
+}
+
+function renderFoodItems(items) {
+  const htmlBuffers = {
+    Burger: "",
+    Pizza: "",
+    Nudeln: "",
+    Beilagen: "",
+  };
+
+  for (let i = 0; i < items.length; i++) {
+    const item = items[i];
+    if (htmlBuffers[item.category] !== undefined) {
+      htmlBuffers[item.category] += getFoodTemplate(item);
+    }
+  }
+
+  for (let category in htmlBuffers) {
+    if (categoryRefs[category]) {
+      categoryRefs[category].innerHTML = `
+        <h3 class="category-headline">${category}</h3>
+        ${htmlBuffers[category]}
+      `;
     }
   }
 }
+
+foodData();
 
 foodData();
