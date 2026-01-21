d2130f49e54a588b30090b2ff7406004e148c70c
JensBaumannDev <baumannjens@outlook.de>
2026-01-21 11:54:59 +0100

    added img to food card, change foodcard style

--- scripts/db.js
+++ scripts/db.js
@@ -5,6 +5,7 @@ let foodDataBase = [
     "name": "Classic Hamburger",
     "price": 7.50,
     "description": "Saftiges Rindfleischpatty mit frischem Salat, Tomaten, Gurken und unserer Haus-Sauce.",
+    "amount" : 0,
     "imgSrc": "./assets/img/burger/hamburger.jpg"
   },
   {
@@ -12,104 +13,134 @@ let foodDataBase = [
     "category": "Burger",
     "name": "Cheeseburger",
     "price": 8.20,
-    "description": "Wie der Klassiker, aber verfeinert mit einer Scheibe geschmolzenem Cheddar-Käse."
+    "description": "Wie der Klassiker, aber verfeinert mit einer Scheibe geschmolzenem Cheddar-Käse.",
+    "amount" : 0,
+    "imgSrc": "./assets/img/burger/cheeseburger.jpg"
   },
   {
     "id": 3,
     "category": "Burger",
     "name": "BBQ Bacon Burger",
     "price": 9.50,
-    "description": "Mit knusprigem Bacon, Röstzwiebeln, sauren Gurken und rauchiger BBQ-Sauce."
+    "description": "Mit knusprigem Bacon, Röstzwiebeln, sauren Gurken und rauchiger BBQ-Sauce.",
+    "amount" : 0,
+    "imgSrc": "./assets/img/burger/bbqbaconburger.jpg"
   },
   {
     "id": 4,
     "category": "Burger",
     "name": "Chicken Burger",
     "price": 8.50,
-    "description": "Knusprig paniertes Hähnchenfilet mit Eisbergsalat und leichter Remoulade."
+    "description": "Knusprig paniertes Hähnchenfilet mit Eisbergsalat und leichter Remoulade.",
+    "amount" : 0,
+    "imgSrc": "./assets/img/burger/chicken-burger.jpg"
   },
   {
     "id": 5,
     "category": "Pizza",
     "name": "Pizza Margherita",
     "price": 8.50,
-    "description": "Der Klassiker mit fruchtiger Tomatensauce, Mozzarella und frischem Basilikum."
+    "description": "Der Klassiker mit fruchtiger Tomatensauce, Mozzarella und frischem Basilikum.",
+    "amount" : 0,
+    "imgSrc": "./assets/img/pizza/pizzamargherita.jpg"
   },
   {
     "id": 6,
     "category": "Pizza",
     "name": "Pizza Salami",
     "price": 9.90,
-    "description": "Herzhaft belegt mit würziger Salami, Tomatensauce und Gouda-Käse."
+    "description": "Herzhaft belegt mit würziger Salami, Tomatensauce und Gouda-Käse.",
+    "amount" : 0,
+    "imgSrc": "./assets/img/pizza/pizzasalami.jpg"
   },
   {
     "id": 7,
     "category": "Pizza",
     "name": "Pizza Prosciutto e Funghi",
     "price": 10.50,
-    "description": "Mit italienischem Hinterschinken, frischen Champignons und Mozzarella."
+    "description": "Mit italienischem Hinterschinken, frischen Champignons und Mozzarella.",
+    "amount" : 0,
+    "imgSrc": "./assets/img/pizza/pizzafunghi.jpg"
   },
   {
     "id": 8,
     "category": "Pizza",
     "name": "Pizza Quattro Formaggi",
     "price": 11.20,
-    "description": "Ein Traum für Käseliebhaber mit Mozzarella, Gorgonzola, Parmesan und Emmentaler."
+    "description": "Ein Traum für Käseliebhaber mit Mozzarella, Gorgonzola, Parmesan und Emmentaler.",
+    "amount" : 0,
+    "imgSrc": "./assets/img/pizza/pizzaformaggi.jpg"
   },
   {
     "id": 9,
     "category": "Nudeln",
     "name": "Spaghetti Bolognese",
     "price": 9.50,
-    "description": "Klassische Pasta mit hausgemachter Hackfleischsauce vom Rind."
+    "description": "Klassische Pasta mit hausgemachter Hackfleischsauce vom Rind.",
+    "amount" : 0,
+    "imgSrc": "./assets/img/nudeln/spaghettibolognese.jpg"
   },
   {
     "id": 10,
     "category": "Nudeln",
     "name": "Spaghetti Carbonara",
     "price": 10.90,
-    "description": "Mit gebratenem Speck, Eigelb, Sahnesauce und viel Parmesan."
+    "description": "Mit gebratenem Speck, Eigelb, Sahnesauce und viel Parmesan.",
+    "amount" : 0,
+    "imgSrc": "./assets/img/nudeln/spaghetticarbonara.jpg"
   },
   {
     "id": 11,
     "category": "Nudeln",
     "name": "Penne Arrabbiata",
     "price": 9.20,
-    "description": "Feurige Tomatensauce mit Knoblauch, Chili und Petersilie (scharf)."
+    "description": "Feurige Tomatensauce mit Knoblauch, Chili und Petersilie (scharf).",
+    "amount" : 0,
+    "imgSrc": "./assets/img/nudeln/pennearrabbiata.jpg"
   },
   {
     "id": 12,
     "category": "Nudeln",
     "name": "Lasagne al Forno",
     "price": 11.50,
-    "description": "Geschichtete Nudelplatten mit Hackfleischsauce und Béchamel, mit Käse überbacken."
+    "description": "Geschichtete Nudelplatten mit Hackfleischsauce und Béchamel, mit Käse überbacken.",
+    "amount" : 0,
+    "imgSrc": "./assets/img/nudeln/lasagne-al-forno.jpg"
   },
   {
     "id": 13,
     "category": "Beilagen",
     "name": "Pizzabrötchen (6 Stück)",
     "price": 4.50,
-    "description": "Frisch aus dem Ofen, wahlweise mit Kräuterbutter oder Aioli serviert."
+    "description": "Frisch aus dem Ofen, wahlweise mit Kräuterbutter oder Aioli serviert.",
+    "amount" : 0,
+    "imgSrc": "./assets/img/beilagen/pizzabrötchen.jpg"
   },
   {
     "id": 14,
     "category": "Beilagen",
     "name": "Insalata Mista",
     "price": 6.90,
-    "description": "Gemischter Salat mit Tomaten, Gurken, Mais, Zwiebeln und Hausdressing."
+    "description": "Gemischter Salat mit Tomaten, Gurken, Mais, Zwiebeln und Hausdressing.",
+    "amount" : 0,
+    "imgSrc": "./assets/img/beilagen/insalata mista.jpg"
   },
   {
     "id": 15,
     "category": "Beilagen",
     "name": "Bruschetta",
     "price": 5.50,
-    "description": "Geröstetes Weißbrot mit gewürfelten Tomaten, Knoblauch, Basilikum und Olivenöl."
+    "description": "Geröstetes Weißbrot mit gewürfelten Tomaten, Knoblauch, Basilikum und Olivenöl.",
+    "amount" : 0,
+    "imgSrc": "./assets/img/beilagen/bruschetta.jpg"
   },
   {
     "id": 16,
     "category": "Beilagen",
     "name": "Portion Oliven",
     "price": 3.90,
-    "description": "Grüne und schwarze Oliven, in Öl mit mediterranen Kräutern eingelegt."
+    "description": "Grüne und schwarze Oliven, in Öl mit mediterranen Kräutern eingelegt.",
+    "amount" : 0,
+    "imgSrc": "./assets/img/beilagen/portionoliven.jpg"
   }
 ];
